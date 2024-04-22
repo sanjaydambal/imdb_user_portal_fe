5ddf7c7cfa1e0b59ca07ccf0c7f514621a6f2f97
sanjaydambal <sanjaydambal@gmail.com>
2024-04-22 23:33:22 +0530

    provided error message pop up for login component

--- src/compoonents/Login.js
+++ src/compoonents/Login.js
@@ -7,6 +7,7 @@ const Login = () => {
         usernameOremail: '',
         password: ''
     });
+    const [showErrorModal,setShowErrorModal] = useState(false)
     const apiUrl = process.env.REACT_APP_API_BASE_URL || "http://localhost:4001";
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -20,9 +21,12 @@ const Login = () => {
             // Handle successful login (e.g., redirect to dashboard)
         } catch (error) {
             console.error('Error logging in:', error.response.data.error);
+            setShowErrorModal(true)
         }
     };
-
+    const handleCancel = () => {
+        setShowErrorModal(false)
+    }
     return (
         <div className="login-container">
             <h2 className="login-title">Login</h2>
@@ -37,6 +41,24 @@ const Login = () => {
                 </div>
                 <button type="submit">Login</button>
             </form>
+            {showErrorModal && (
+                <div className="modal-overlay">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h2 className="modal-title">Email or password is not matching</h2>
+                            </div>
+                            <div className="modal-body">
+                                <p>Please provide proper login credentials </p>
+                            </div>
+                            <div className="modal-footer">
+                                
+                                <button className="btn btn-secondary" onClick={handleCancel}>Cancel</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
